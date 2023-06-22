c9977de8ec20951eab782603bd36d4f43dbed9f7
pablokaue2004 <pablokauedev23@gmail.com>
2023-06-22 07:41:36 -0300

    aparentemente finalizadp

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,14 +1,11 @@
 import React, {createContext, useState, useEffect, useContext} from 'react'
 import * as auth from '../services/auth'
 
-//interface para tipar o conteudo 
-//interface User, para definir o objeto
 interface User{
     name: string;
     email: string;
 }
 
-//interface AuthContextData, para definir o tipo de conteudo do Context
 interface AuthContexData{
     signed: boolean;
     user: User | null;
@@ -19,10 +16,8 @@ interface AuthContexData{
 
 /************ */
 
-//definindo o contexto, e passando a interface como tipo <Tipo>
 const AuthContext = createContext<AuthContexData>({} as AuthContexData)
 
-//Definindo o provider que ficara por volta da aplicacao -- o children são os filhos que ficaram por dentro desse componente, no App.tsx
 export const AuthProvider: React.FC = ({children}) => {
     const [user, setUser] = useState<User | null>(null)
     const [load, setLoad] = useState<boolean>(false)
@@ -55,19 +50,14 @@ export const AuthProvider: React.FC = ({children}) => {
     }
 
     return(
-        //ao usar !! transforma a variavel em boolean, mesmo que usar Boolean(user)
-        //no value sao enviados as variaveis ou funcoes que poderam ser usadas nos componentes filhos
         <AuthContext.Provider value={{signed: !!user, signIn, user, logOut, load}}>
             {children}
         </ AuthContext.Provider>
     )
 }
 
-
-//criando um hook para usar nos componentes
 export function useAuth(){
     const context = useContext(AuthContext)
 
-    //dentro do context de rotorno estao as variaveis que podem ser acessadas usando o useAuth
     return context
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,3 @@
-//interface Response usada como tipo da promise de retorno
 interface Response{
     token: string;
     user: {
@@ -7,15 +6,14 @@ interface Response{
     };
 }
 
-//aqui podemos definir um retorno para a função , :Promisse<tipoResponse>, retorna uma promise do tipo Response
 export function signIn():Promise<Response> {
     return new Promise(resolve=>{
         setTimeout(() => {
             resolve({
                 token: 'esse_e_meu_token_secreto_56524$$##2',
                 user: {
-                    name: 'Chromium',
-                    email: 'chromium@js.com'
+                    name: 'kaue',
+                    email: 'kaue@gmail.com'
                 }
             })
         }, 2000);
